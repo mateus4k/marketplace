0e0feaa15973f3027721f3f2a2bdb48292223a02
mateus4k <mateus4k@protonmail.ch>
2019-09-15 14:56:45 -0300

    :rocket: Email template

--- README.md
+++ README.md
@@ -5,4 +5,9 @@
 ```sh
 # install MongoDB on Docker
 docker run --name mongonode -p 27017:27017 -d -t mongo
+
+# run server
+yarn start
+# or
+npm run start
 ```
--- src/app/controllers/PurchaseController.js
+++ src/app/controllers/PurchaseController.js
@@ -12,7 +12,8 @@ class PurchaseController {
       from: '"Mateus Sampaio" <mateus4k@protonmail>',
       to: purchaseAd.author.email,
       subject: `Solicitação de compra: ${purchaseAd.title}`,
-      html: `<p>Teste: ${content}</p>`
+      template: "purchase",
+      context: { user, content, ad: purchaseAd }
     });
 
     return res.send();
