95961293166fffc2e8f6ec92f048de32d66c3eed
mateus4k <mateus4k@protonmail.ch>
2019-09-17 23:42:48 -0300

    :zap: Sentry!

--- src/app/services/Queue.js
+++ src/app/services/Queue.js
@@ -1,4 +1,5 @@
 const kue = require("kue");
+const Sentry = require("@sentry/node");
 const redisConfig = require("../../config/redis");
 const jobs = require("../jobs");
 
@@ -6,5 +7,6 @@ const Queue = kue.createQueue({ redis: redisConfig });
 
 Queue.process(jobs.PurchaseMail.key, jobs.PurchaseMail.handle);
 
+Queue.on("error", Sentry.captureException);
+
 module.exports = Queue;
- 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+module.exports = {
+  dsn: "https://586ef15566ba42f1a7aadb78fa15281b@sentry.io/1730150"
+};
